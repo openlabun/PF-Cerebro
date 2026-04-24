3610fcff67bb00802a9ec1e902e6d698e352a76d
torrober <robemario124@gmail.com>
2026-04-23 23:32:11 -0500

    Ui fixes 2

--- Movil/cerebros-app/pages/AuthRequiredPage.tsx
+++ Movil/cerebros-app/pages/AuthRequiredPage.tsx
@@ -115,8 +115,8 @@ const styles = StyleSheet.create({
     paddingVertical: 24,
   },
   logo: {
-    width: 320,
-    height: 320,
+    width: 100,
+    height: 100,
     marginBottom: 18,
   },
   title: {
